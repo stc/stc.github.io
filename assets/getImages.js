c2fa4024b9ca6df48fae017d27668ef1e87e7330
Agoston Nagy <stc@binaura.net>
2024-08-26 14:02:36 +0200

    fade on hover

--- assets/getImages.js
+++ assets/getImages.js
@@ -1,25 +1,26 @@
 const imageList = [
-    ['./assets/img/dotwork.png','https://www.fxhash.xyz/generative/slug/dotwork'],
-    ['./assets/img/echo.png','https://www.binaura.net/echo'],
-    ['./assets/img/gr1dflow.png','https://highlight.xyz/mint/660573d2f9176ca33f52f34f'],
-    ['./assets/img/iss.png','https://www.binaura.net/stc/sand-sort'],
-    ['./assets/img/l-poem.png','https://www.fxhash.xyz/generative/25977'],
-    ['./assets/img/m0-n0.png','https://www.fxhash.xyz/generative/slug/m0-n0'],
-    ['./assets/img/proc.png','https://www.expanded.art/collections/agoston-nagy-procedural-drawings'],
-    ['./assets/img/re-frame.png','https://highlight.xyz/mint/66ae4b90a963922a3a55935a'],
-    ['./assets/img/tur1ng.png','https://www.fxhash.xyz/generative/slug/tur1ng']
+    ['./assets/img/dotwork.png', 'https://www.fxhash.xyz/generative/slug/dotwork','dotwork'],
+    ['./assets/img/echo.png', 'https://www.binaura.net/echo','echo'],
+    ['./assets/img/gr1dflow.png', 'https://highlight.xyz/mint/660573d2f9176ca33f52f34f','gr1dflow'],
+    ['./assets/img/iss.png', 'https://www.binaura.net/stc/sand-sort','infinite sand sorter'],
+    ['./assets/img/l-poem.png', 'https://www.fxhash.xyz/generative/25977','L-Poem'],
+    ['./assets/img/m0-n0.png', 'https://www.fxhash.xyz/generative/slug/m0-n0','m0-n0'],
+    ['./assets/img/proc.png', 'https://www.expanded.art/collections/agoston-nagy-procedural-drawings','Procedural Drawings'],
+    ['./assets/img/re-frame.png', 'https://highlight.xyz/mint/66ae4b90a963922a3a55935a','re-frame'],
+    ['./assets/img/tur1ng.png', 'https://www.fxhash.xyz/generative/slug/tur1ng','tur1ng']
 ];
 
 function getRandomImages() {
     const randomImages = [];
     const usedIndexes = new Set();
-    
-    while(randomImages.length < 3) {
+
+    while (randomImages.length < 3) {
         const randomIndex = Math.floor(Math.random() * imageList.length);
         if (!usedIndexes.has(randomIndex)) {
             let arr = [];
             arr.push(imageList[randomIndex][0]);
-            arr.push(imageList[randomIndex][1])
+            arr.push(imageList[randomIndex][1]);
+            arr.push(imageList[randomIndex][2]);
             randomImages.push(arr);
             usedIndexes.add(randomIndex);
         }
@@ -31,22 +32,37 @@ function displayImages() {
     const imageContainer = document.getElementById('image-container');
     imageContainer.innerHTML = '';
     const randomImages = getRandomImages();
-    
-    randomImages.forEach(imageData => {
-        const anchorElement = document.createElement('a');
-                anchorElement.href = imageData[1]; 
-                anchorElement.target = '_self'; 
 
-                const imgElement = document.createElement('img');
-                imgElement.src = imageData[0];
-                imgElement.style.width = "30%"; 
-                imgElement.style.marginTop = "0px";
-                imgElement.style.marginLeft = "0px";
-                imgElement.style.marginRight = "10px";  
-                imgElement.style.marginBottom = "10px";  
-                
-                anchorElement.appendChild(imgElement);
-                imageContainer.appendChild(anchorElement);
+    randomImages.forEach(imageData => {
+    
+        const imgElement = document.createElement('img');
+        imgElement.src = imageData[0];
+        imgElement.style.width = "100%";
+        imgElement.style.marginTop = "0px";
+        imgElement.style.marginLeft = "0px";
+        imgElement.style.marginRight = "0px";
+        imgElement.style.marginBottom = "0px";
+
+        const divElement = document.createElement('div');
+        divElement.className = "divElement";
+        divElement.style.width = "30%";
+        divElement.style.backgroundColor = "#ff0000"
+        divElement.style.marginTop = "0px";
+        divElement.style.marginLeft = "0px";
+        divElement.style.marginRight = "10px";
+        divElement.style.marginBottom = "10px";
+        
+        const anchorElement = document.createElement('a');
+        anchorElement.href = imageData[1];
+        anchorElement.target = '_self';
+        
+        divElement.appendChild(imgElement);
+        const overlayElement = document.createElement('div');
+        overlayElement.className = "overlay";
+        //overlayElement.innerHTML = imageData[2];
+        anchorElement.appendChild(overlayElement);
+        divElement.appendChild(anchorElement);
+        imageContainer.appendChild(divElement);
     });
 }
 
